cf22ba66bf7f430af5d15559fe4f736856a77117
llckumadevelop <kumauser01@kuma.ink>
2024-01-19 22:49:51 +0700

    update code

--- App.tsx
+++ App.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import { Text } from 'react-native';
 import HomeScreen from './screens/HomeScreen'
-
-import Icon from 'react-native-vector-icons/AntDesign';
 
 const App = () => {
   return (
